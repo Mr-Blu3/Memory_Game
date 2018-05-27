2121ad4743de05ec8450b768a180bfba225554cb
mr-blu3 <pontusp66j@gmail.com>
2018-05-27 14:54:48 +0200

    init proj

---
+++ types/react-redux.d.ts
@@ -0,0 +1,11 @@
+import 'react-redux';
+
+declare module 'react-redux' {
+
+  export function connect<TStateProps = any, TDispatchProps = any, TOwnProps = any, TMergedProps = any>(
+    mapStateToProps: MapStateToPropsParam<TStateProps, TOwnProps>,
+    mapDispatchToProps?: MapDispatchToPropsParam<TDispatchProps, TOwnProps>,
+    mergeProps?: MergeProps<TStateProps, TDispatchProps, TOwnProps, TMergedProps>,
+    options?: Options
+  );
+}
